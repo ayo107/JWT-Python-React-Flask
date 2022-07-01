7fc7e52d081f76b0ce799d0e310d90ab27150c9d
ayo107 <ayo_linkin10@hotmail.com>
2022-07-01 10:09:40 +0000

    primera subida

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,16 +3,26 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav className="navbar navbar-light bg-light mb-3">
+			<Link to="/">
+				<span className="navbar-brand mb-0 h1">
+					<img
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRsnUXkrv5QBuWx6TYrKxNqIXa5JPdUhwLfAzN432_Dg3HyrnFEc-Y79Kl7wonvg5pP4E&usqp=CAU"
+						width="60"
+						height="40"
+					/>
+				</span>
+			</Link>
+			<div className="ml-auto">
+				<Link to="/protected">
+					<button className="btn btn-primary">User Zone</button>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+				<Link to="/login">
+					<button className="btn btn-primary">Login</button>
+				</Link>
+				<Link to="/signup">
+					<button className="btn btn-primary">Sign Up!</button>
+				</Link>
 			</div>
 		</nav>
 	);
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,6 +9,10 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Login } from "./pages/login";
+import { Signup } from "./pages/signup";
+import {Notlogin} from "./pages/errorLogin"
+import { Protected } from "./pages/protected";
 
 //create your first component
 const Layout = () => {
@@ -28,8 +32,20 @@ const Layout = () => {
 						<Route exact path="/demo">
 							<Demo />
 						</Route>
-						<Route exact path="/single/:theid">
+						<Route exact path="/single">
 							<Single />
+						</Route>
+						<Route exact path="/notlogin">
+							<Notlogin />
+						</Route>
+						<Route exact path="/login">
+							<Login />
+						</Route>
+						<Route exact path="/signup">
+							<Signup />
+						</Route>
+						<Route exact path="/protected">
+							<Protected />
 						</Route>
 						<Route>
 							<h1>Not found!</h1>
